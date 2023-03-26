da1aae99a78b113137ded6f52597d05498278a6a
summrheat <artur.aminov.01@mail.ru>
2023-03-26 17:42:28 +0500

    Законченная 2 практика. Добавлены классы GitHubParser и StackOverflowParser для обработки ссылок

--- MavenProject/link-parser/src/main/java/ru/tinkoff/edu/java/parser/GitHubParser.java
+++ MavenProject/link-parser/src/main/java/ru/tinkoff/edu/java/parser/GitHubParser.java
@@ -1,16 +1,34 @@
 package ru.tinkoff.edu.java.parser;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.IOException;
-import java.net.URL;
-
+// Класс для обработки ссылки с GitHub
 public class GitHubParser {
-    public static JsonNode get(URL url) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readTree(url);
+
+    private String login;
+    private String repo;
+
+    // Конструктор, который принимает ссылку в виде строки и разбивает ее на части
+    public GitHubParser(String link) {
+        // Проверяем, что ссылка не пустая и начинается с https://github.com/
+        if (link == null || link.isEmpty() || !link.startsWith("https://github.com/")) {
+            throw new IllegalArgumentException("Неверный формат ссылки");
+        }
+        // Удаляем префикс https://github.com/ и разделяем оставшуюся часть по символу /
+        String[] parts = link.substring(19).split("/");
+        // Проверяем, что получили две части: логин и название репозитория
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Неверный формат ссылки");
+        }
+        // Присваиваем полям значения из частей
+        login = parts[0];
+        repo = parts[1];
     }
 
-}
 
+    public String getLogin() {
+        return login;
+    }
+
+    public String getRepo() {
+        return repo;
+    }
+}
